921086d5ee36dcd5c99084c7f1901bbc5e2282e5
ppillot <paulpillot@gmail.com>
2019-09-08 00:49:34 -0400

    component commandsAtom

---
+++ src/components/Commands.Atom.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <fieldset>
+        <legend>
+            Afficher atomes
+        </legend>
+        <div class="button" @click="displayAtom('sphere')">
+            <img src="img/cpk100pc.gif" />
+            <br> Sphères
+        </div>
+        <div class="button" @click="displayAtom('bs')"><img src="img/cpk20pc.gif" />
+            <br> Sphères 20%</div>
+        <div class="button" @click="displayAtom('none')"><img src="img/noir.gif" />
+            <br> Effacer
+        </div>
+    </fieldset>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Mutations from '../mutations'
+import { AtomDisplay } from '../utils/types'
+
+export default Vue.extend({
+  name: 'CommandsAtom',
+  data () {
+    return {
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    displayAtom (type: AtomDisplay) {
+      this.$store.commit(Mutations.ATOM_DISPLAYED, type)
+    }
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.button {
+    display: inline-block;
+    font-size: 11px;
+    cursor: pointer;
+    text-align: center;
+    margin-right: 10px;
+    img {
+        height: 40px;
+        width: 40px;
+        border: none;
+        border-radius: 3px;
+        box-shadow: 1px 1px 3px #aaa;
+    }
+}
+fieldset {
+    border: 1px solid #CFD8DC;
+    border-radius: 2px;
+}
+</style>
